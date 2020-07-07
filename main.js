685a40670eed87c09f4c492b8bc3fabf41c38640
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-07-07 12:17:59 -0400

    4 filter functions created with FOR..OF loops

--- main.js
+++ main.js
@@ -2,6 +2,60 @@
  * YOUR CODE HERE *
  ******************/
 
+const onlyOdds = function (nums){
+  oddNums = []
+  for (number of nums){
+    if (number % 2 !== 0) {
+      oddNums.push(number)
+    }
+  }
+  return oddNums
+}
+//___________________________________________________________________//
+//___________________________________________________________________//
+
+
+const onlyEvens = function (nums){
+  evenNums = []
+  for (number of nums){
+    if(number % 2 === 0) {
+      evenNums.push(number)
+    }
+  }
+  return evenNums
+}
+//___________________________________________________________________//
+//___________________________________________________________________//
+
+
+
+const shortNamesOnly = function(names){
+  namesWithLessThan7Char = []
+  for (name of names){
+    if (name.length < 7){
+      namesWithLessThan7Char.push(name)
+    }
+  }
+  return namesWithLessThan7Char;
+}
+//___________________________________________________________________//
+//___________________________________________________________________//
+
+
+
+const dNames = function(names){
+  namesThatStartWithD = []
+  for (name of names){
+    if (name[0] === 'D'){
+      namesThatStartWithD.push(name)
+    }
+  }
+  return namesThatStartWithD
+}
+//___________________________________________________________________//
+//___________________________________________________________________//
+
+
 
 
 
